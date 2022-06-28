d8ddd6edb0a99369d6fdd40772070d25dd1c99c6
Hasanul-Alam <superdev0000@gmail.com>
2022-06-28 09:37:07 +0600

    Balance field handled without function

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,18 @@
-console.log('welcome to my functional bank');
+document.getElementById('deposit-submit-btn').addEventListener('click', function(){
+    const inputText = document.getElementById('deposit-field');
+    const inputValue = parseFloat(inputText.value);
+
+    // Get the current deposit amount
+    const currentDeposit = document.getElementById('deposit-amount');
+    const currentDepositValue = parseFloat(currentDeposit.innerText);
+
+    // Set input value to deposit amount
+    currentDeposit.innerText = inputValue + currentDepositValue;
+
+    // Update balance
+    const currentBalance = document.getElementById('balance-amount');
+    const currentBalanceValue = parseFloat(currentBalance.innerText);
+    currentBalance.innerText = currentBalanceValue + inputValue;
+    // Clear deposit input field
+    inputText.value = '';
+});
